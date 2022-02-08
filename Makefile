279152cdb964b58d44b8d38c76c3a772734d6238
mamosca <marc.mosca@icloud.com>
2022-02-08 09:26:26 +0100

    feat: signals ctrl+\ and other files for pull

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jbosquet <jbosquet@student.42.fr>          +#+  +:+       +#+         #
+#    By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/02/05 17:07:00 by mmosca            #+#    #+#              #
-#    Updated: 2022/02/06 15:58:42 by jbosquet         ###   ########.fr        #
+#    Updated: 2022/02/08 09:23:46 by mmosca           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,7 @@ include		config/compilations.mk
 include		config/prints.mk
 include		config/properties.mk
 include		config/sources.mk
+include		config/sources2.mk
 
 ##	~~	Configurations ---------------------------------------------------------
 
--- includes/minishell2.h
+++ includes/minishell2.h
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signals.c                                          :+:      :+:    :+:   */
+/*   minishell2.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/07 10:47:02 by mmosca            #+#    #+#             */
-/*   Updated: 2022/02/08 09:25:41 by mmosca           ###   ########lyon.fr   */
+/*   Created: 2022/02/08 09:24:20 by mmosca            #+#    #+#             */
+/*   Updated: 2022/02/08 09:24:27 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static void
-	handle_signals(int signo)
-{
-	(void) signo;
-	// kill ?
-	printf("\n");
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-}
-
-void
-	my_signal(void)
-{
-	if (signal(SIGINT, handle_signals) == SIG_ERR \
-	OR signal(SIGQUIT, SIG_IGN) == SIG_ERR)
-		error("bad signals", 1);
-}
