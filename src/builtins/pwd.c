4656e0fa9c952d78e3f1e0f45b09f239833808f0
mamosca <marc.mosca@icloud.com>
2022-02-10 12:54:01 +0100

    feat: change pwd builtins

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,25 +6,38 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 10:30:39 by mmosca            #+#    #+#             */
-/*   Updated: 2022/02/09 18:27:13 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/02/10 12:53:16 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell2.h"
 
+// void
+// 	builtins_pwd(t_minishell *minishell)
+// {
+// 	char	*working_directory;
+// 	int		line;
+
+// 	line = find_line_of_name(minishell->environnement, "PWD", \
+// 	minishell->garbage);
+// 	if (line == -1)
+// 		return ;
+// 	working_directory = get_value_of_line(minishell->environnement[line], \
+// 	minishell->garbage);
+// 	printf("%s\n", working_directory);
+// 	free(working_directory);
+// 	minishell->exit_code = 0;
+// }
+
 void
 	builtins_pwd(t_minishell *minishell)
 {
-	char	*working_directory;
-	int		line;
+	char	*pwd;
 
-	line = find_line_of_name(minishell->environnement, "PWD", \
-	minishell->garbage);
-	if (line == -1)
+	pwd = getcwd(NULL, 0);
+	if (pwd == NULL)
 		return ;
-	working_directory = get_value_of_line(minishell->environnement[line], \
-	minishell->garbage);
-	printf("%s\n", working_directory);
-	free(working_directory);
+	printf("%s\n", pwd);
+	free(pwd);
 	minishell->exit_code = 0;
 }
