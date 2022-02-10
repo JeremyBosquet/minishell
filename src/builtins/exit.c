3cc120db56e3cf22eca0bd8b0ff5018b00644d45
mamosca <marc.mosca@icloud.com>
2022-02-10 15:46:19 +0100

    feat: too many argument in exit builtin

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,20 +6,25 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 09:40:02 by mmosca            #+#    #+#             */
-/*   Updated: 2022/02/10 13:22:57 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/02/10 15:42:07 by mmosca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell2.h"
 
-/*
- * Si tu as un argument qui est plus grand de 256 alors faut retirer 256
- * a l'argument passer en parametre
- */
 void
 	builtins_exit(t_minishell *minishell, int i)
 {
-	// Faire attention car il print le exit mais n'exit pas du prog.
+	int	j;
+
+	j = 0;
+	while (minishell->commands[i].command[j])
+		j += 1;
+	if (j > 2)
+	{
+		printf("exit\ncouscous: exit: too many arguments\n");
+		return ;
+	}
 	if (minishell->commands[i].command[1] != NULL)
 	{
 		minishell->exit_code = ft_atoi(minishell->commands[i].command[1]);
